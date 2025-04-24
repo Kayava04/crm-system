fa572cf43f00f15c810f25665c9f00cc612f7284
Bohdan <bogdan.misko85@gmail.com>
2025-04-25 00:52:34 +0300

    Add: added field 'role' for user response

--- Backend/CRMSystem.WebAPI/DTOs/User/UserResponseDto.cs
+++ Backend/CRMSystem.WebAPI/DTOs/User/UserResponseDto.cs
@@ -7,6 +7,7 @@ namespace CRMSystem.WebAPI.DTOs.User
         public DateTime BirthDate { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
+        public int RoleId { get; set; }
         public string? ImageUrl { get; set; }
     }
 }
